74be5af2bd636bf86f194e702d232ea2ffe283af
rasssya76 <80687852+rasssya76@users.noreply.github.com>
2022-02-06 08:35:10 +0700

    Update confat.js

--- confat.js
+++ confat.js
@@ -4,9 +4,9 @@ const setting = JSON.parse(fs.readFileSync('./setting.json'));
 
 // Other
 global.owner = ["6281515589573","6281515589573"]
-global.packname = ''
+global.packname = 'R-BOTxMdོ'
 global.youtube = 'https://cutt.ly/5UFQN1L'
-global.author = '© R-BOT MD'
+global.author = 'i'm Rama 々/>'
 global.sessionName = 'session'
 global.flaming = "https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text="
 global.prefa = ['','!','.','🐦','🐤','🗿','#','/','@','&','^','_',',']
